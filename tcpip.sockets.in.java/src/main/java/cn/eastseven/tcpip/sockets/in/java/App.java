4efa11c90a5232084d974ff5d105fb57093ebbee
eastseven <eastseven.dongq@gmail.com>
2013-10-30 09:33:32 +0800

    init

---
+++ tcpip.sockets.in.java/src/main/java/cn/eastseven/tcpip/sockets/in/java/App.java
@@ -0,0 +1,15 @@
+package cn.eastseven.tcpip.sockets.in.java;
+
+import org.apache.log4j.Logger;
+
+
+public class App {
+	
+	static Logger log = Logger.getLogger(App.class.getName());
+	
+	public static void main(String[] args) {
+		log.info("start...");
+		
+		log.info("end...");
+	}
+}
